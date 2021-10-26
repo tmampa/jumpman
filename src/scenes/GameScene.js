f1ad1d65563d7201aa70a59522b7bd1e315ebe83
tmampa <tmampa94@gmail.com>
2021-10-26 13:10:17 +0200

    Default game scene template

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,18 @@
+import  Phaser from 'Phaser';
+
+export default class GameScene extends Phaser.scene
+{
+    constructor() {
+        super('game-scene');
+    }
+
+    preload ()
+    {
+
+    }
+
+    create ()
+    {
+
+    }
+}
