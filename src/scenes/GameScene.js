8f8c1747940a0d436ea7ca4217965b4e952d3aa6
tmampa <tmampa94@gmail.com>
2021-10-27 14:10:43 +0200

    Create platforms

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -10,6 +10,7 @@ export default class GameScene extends Phaser.Scene
     preload()
     {
         this.load.image('sky', '../public/assets/sky.png')
+
         this.load.image('ground', '../public/assets/platform.png')
         this.load.image('apple', '../public/assets/apple.png')
         this.load.image('bomb', '../public/assets/bomb.png')
@@ -23,6 +24,18 @@ export default class GameScene extends Phaser.Scene
     create()
     {
         this.add.image(400, 300, 'sky')
-        this.add.image(400, 300, 'apple')
+        // this.add.image(400, 300, 'apple')
+        this.createPlatforms()
+    }
+
+    createPlatforms()
+    {
+        const platforms = this.physics.add.staticGroup()
+
+        platforms.create(400, 568, 'ground').setScale(2).refreshBody()
+
+        platforms.create(600, 400, 'ground')
+        platforms.create(50, 250, 'ground')
+        platforms.create(750, 220, 'ground')
     }
 }
