ffed4e7a1a759cf38548b6535c5b1f660ede90e2
tmampa <tmampa94@gmail.com>
2021-10-28 02:43:59 +0200

    Create player

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,12 +1,14 @@
 import Phaser from 'phaser'
 
 const GROUND_KEY = 'ground'
+const  DUDE_KEY = 'hero'
 
 export default class GameScene extends Phaser.Scene
 {
     constructor()
     {
         super('game-scene')
+        this.player = undefined
     }
 
     preload()
@@ -17,8 +19,8 @@ export default class GameScene extends Phaser.Scene
         this.load.image('apple', '../public/assets/apple.png')
         this.load.image('bomb', '../public/assets/bomb.png')
 
-        this.load.spritesheet('dude',
-            'assets/dude.png',
+        this.load.spritesheet(DUDE_KEY,
+            '../public/assets/dude.png',
             { frameWidth: 32, frameHeight: 48 }
         )
     }
@@ -28,6 +30,7 @@ export default class GameScene extends Phaser.Scene
         this.add.image(400, 300, 'sky')
         // this.add.image(400, 300, 'apple')
         this.createPlatforms()
+        this.createPlayer()
     }
 
     createPlatforms()
@@ -39,5 +42,32 @@ export default class GameScene extends Phaser.Scene
         platforms.create(600, 400, GROUND_KEY)
         platforms.create(50, 250, GROUND_KEY)
         platforms.create(750, 220, GROUND_KEY)
+    }
+
+    createPlayer()
+    {
+        this.player = this.physics.add.sprite(100, 450, DUDE_KEY)
+        this.player.setBounce(0.2)
+        this.player.setCollideWorldBounds(true)
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers(DUDE_KEY, { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: DUDE_KEY, frame: 4 } ],
+            frameRate: 20
+        })
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers(DUDE_KEY, { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        })
     }
 }
